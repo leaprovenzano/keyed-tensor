4d9c1ddaf9dcc41b7b35cc28dcdb2125894eef81
lea <leaprovenzano@gmail.com>
2020-08-02 16:19:56 +0100

    add test for keyed_reduce

--- tests/test_self_reductions.py
+++ tests/test_self_reductions.py
@@ -25,3 +25,28 @@ def test_all_reduce(funcname):
     method = getattr(kt, funcname)
 
     return method() == torchfunc(kt) == expected
+
+
+@pytest.mark.parametrize('funcname,', self_reductions + bool_reductions)
+def test_keyed_reduce(funcname):
+
+    kt = KeyedTensor(a=torch.rand(5, 4), b=torch.rand(5, 2), c=torch.rand(5))
+    if funcname in bool_reductions:
+        kt.a = kt.a >= 0.5
+        kt.b = kt.b >= 0.5
+        kt.c = kt.c >= 0.5
+
+    torchfunc = getattr(torch, funcname)
+
+    # test as classmethod
+
+    method = getattr(KeyedTensor, funcname)
+    result = method(kt, dim='key')
+    print(kt.a)
+
+    print(result)
+    print(torchfunc(kt.a))
+
+    assert result.a == torchfunc(kt.a)
+    assert result.b == torchfunc(kt.b)
+    assert result.c == torchfunc(kt.c)

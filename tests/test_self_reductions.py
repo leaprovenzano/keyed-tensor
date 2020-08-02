dd25a3f66a66e91c007df161b4451b14aba83a62
lea <leaprovenzano@gmail.com>
2020-08-02 11:17:31 +0100

    add test for all_reduce on self reductions

---
+++ tests/test_self_reductions.py
@@ -0,0 +1,27 @@
+import pytest
+import torch
+from keyedtensor import KeyedTensor
+
+bool_reductions = ['any', 'all']
+self_reductions = ['argmin', 'argmax', 'mean', 'median', 'norm', 'prod', 'var', 'sum']
+
+
+@pytest.mark.parametrize('funcname,', self_reductions + bool_reductions)
+def test_all_reduce(funcname):
+
+    kt = KeyedTensor(a=torch.rand(5, 4), b=torch.rand(5, 2), c=torch.rand(5))
+    if funcname in bool_reductions:
+        kt.a = kt.a >= 0.5
+        kt.b = kt.b >= 0.5
+        kt.c = kt.c >= 0.5
+
+    flatvalues = torch.cat(list(map(torch.flatten, kt.values())))
+
+    torchfunc = getattr(torch, funcname)
+    expected = torchfunc(flatvalues)
+
+    # test as classmethod
+
+    method = getattr(kt, funcname)
+
+    return method() == torchfunc(kt) == expected

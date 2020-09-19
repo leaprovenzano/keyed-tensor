056ec4533d10d835aafb68751a36fcd01c2ac0b1
lea <leaprovenzano@gmail.com>
2020-09-19 12:27:57 +0100

    fix typing_extensions for versions < 3.8
    We should only install typing_extensions if < 3.8. typing extensions was missing in previous release

--- src/keyedtensor/_keyedtensor.py
+++ src/keyedtensor/_keyedtensor.py
@@ -1,5 +1,11 @@
-from typing_extensions import Literal
+import sys
 from typing import List, Union, Optional
+
+if sys.version_info < (3, 8):
+    from typing_extensions import Literal
+else:
+    from typing import Literal
+
 import numpy as np
 
 import torch

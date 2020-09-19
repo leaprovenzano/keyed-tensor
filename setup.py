8cffcf3f4cbc3936a78413c87544f0fae6df6ef7
lea <leaprovenzano@gmail.com>
2020-09-19 11:59:42 +0100

    bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/leaprovenzano/keyedtensor',
-    version='0.0.0',
+    version='0.1.0',
     zip_safe=False,
 )
--- src/keyedtensor/__init__.py
+++ src/keyedtensor/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Lea Provenzano"""
 __email__ = 'leaprovenzano@gmail.com'
-__version__ = '0.0.0'
+__version__ = '0.1.0'
 
 from ._keyedtensor import KeyedTensor

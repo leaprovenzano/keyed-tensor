82070a8cf75546c5d046d9cff03d6e23fbe583f7
lea <leaprovenzano@gmail.com>
2020-09-26 13:59:18 +0100

    fix missing cached_property dependency under py 3.7

--- src/keyedtensor/_keyedsize.py
+++ src/keyedtensor/_keyedsize.py
@@ -1,6 +1,10 @@
+import sys
 from typing import Union
 
-from functools import cached_property
+if sys.version_info < (3, 8):
+    from functools import cached_property
+else:
+    from cached_property import cached_property
 
 
 class KeyedSize:
